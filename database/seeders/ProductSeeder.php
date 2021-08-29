fa802f9f1d32d13d60fbe46ffd6abb4773073885
Paweł Płonka <pjplonka@gmail.com>
2021-08-29 23:48:55 +0200

    Update product seeder.

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,20 +2,30 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
+    public $products = [
+        'Ryż biały (surowy)', 7.4, 0, 79, 0, 0,
+        'Filet z piersi z indyka bez skóry (surowy)', 22, 2, 79, 0, 0,
+    ];
+
     public function run(): void
     {
-        DB::table('products')->insert([
-            'name' => 'Ryż biały (surowy)',
-            'protein' => 7.4,
-            'fat' => 0,
-            'carbohydrates' => 0,
-            'sugar' => 0,
-            'fiber' => 0,
-        ]);
+        foreach ($this->products as $product) {
+            DB::table('products')->insert([
+                'name' => $product[0],
+                'protein' => $product[1],
+                'fat' => $product[2],
+                'carbohydrates' => $product[3],
+                'sugar' => $product[4],
+                'fiber' => $product[5],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
