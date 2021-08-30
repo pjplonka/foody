2dba19995925d08bd6358f22618f300fa1f34564
Paweł Płonka <pjplonka@gmail.com>
2021-08-30 15:46:55 +0200

    Changes.

--- resources/views/days/form.blade.php
+++ resources/views/days/form.blade.php
@@ -2,5 +2,5 @@
     'type' => 'date',
     'id' => 'date',
     'label' => 'Date',
-    'value' => old('date') ?: ($day->date ? $day->date->format('Y-m-d') : '')
+    'value' => old('date') ?: ($day->date ? $day->date->format('Y-m-d') : \Carbon\Carbon::now()->format('Y-m-d'))
 ])
