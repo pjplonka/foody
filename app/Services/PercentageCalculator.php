d510cf5f13b2241303472b2dfc52e59e936cee0d
Paweł Płonka <pjplonka@gmail.com>
2021-08-30 11:37:03 +0200

    Fix my goal view.

--- app/Services/PercentageCalculator.php
+++ app/Services/PercentageCalculator.php
@@ -6,6 +6,8 @@ class PercentageCalculator
 {
     public function rounded($max, $part)
     {
+        echo '<pre>' , var_dump($max, $part) , '</pre>'; die;
+        
         if ($max === 0 || $max === '0') {
             return 0;
         }
